e24be2e62ef1395a93fcc49eed72e7b34a7a6cff
kaekaelala <kaela.leow@gmail.com>
2026-01-18 18:01:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// charm interaction will go here later
+const charmSections = document.querySelectorAll(".charms");
+
+const charms = [
+  "theweeknd.png",
+  "drake.png",
+  "transform.png"
+];
+
+charmSections.forEach(section => {
+  charms.forEach(charm => {
+    const img = document.createElement("img");
+    img.src = `charms/${charm}`;
+    img.onclick = () => {
+      img.classList.toggle("selected");
+    };
+    section.appendChild(img);
+  });
+});
